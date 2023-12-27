a1af0c4561d099bbc32061544e89bded39f58e9f
yusuf <yusuferkoc99@gmail.com>
2023-12-27 15:14:12 +0300

    Initial Commit

---
+++ ChatUI/ChatUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatUIApp.swift
+//  ChatUI
+//
+//  Created by Yusuf Erkoç on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
